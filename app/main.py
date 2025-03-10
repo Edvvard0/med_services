37384ac40bd9fdc73ec8db4ddc936ff05ec30267
Edvvard0 <edward.osipchukzzz@gmail.com>
2025-03-10 22:03:27 +0300

    добавил файлы для работы с web частью

--- app/main.py
+++ app/main.py
@@ -1,9 +1,12 @@
 import uvicorn
 from fastapi import FastAPI
+from starlette.staticfiles import StaticFiles
+
 from app.patients.router import router as patient_router
 from app.doctors.router import router as doctor_router
 from app.hospitalization.router import router as hospitalization_router
 from app.med_procedure.router import router as med_procedure_router
+from app.pages.router import router as pages_router
 
 
 app = FastAPI()
@@ -12,6 +15,9 @@ app.include_router(patient_router)
 app.include_router(doctor_router)
 app.include_router(hospitalization_router)
 app.include_router(med_procedure_router)
+app.include_router(pages_router)
+
+app.mount("/static", StaticFiles(directory="app/static"), "static")
 
 
 @app.get('/')
---
+++ app/pages/router.py
@@ -0,0 +1,15 @@
+from fastapi import APIRouter
+from starlette.requests import Request
+from starlette.responses import HTMLResponse
+from starlette.templating import Jinja2Templates
+
+router = APIRouter(prefix='/pages',
+                   tags=['Pages'])
+
+template = Jinja2Templates(directory='app/templates')
+
+
+@router.get('/')
+async def info_page(request: Request) -> HTMLResponse:
+    return template.TemplateResponse(name='index.html',
+                                     context={'request': request})
